d3aac7e481700790cb73133585c5d8e5e159f542
Joker <99551853+JokHacker@users.noreply.github.com>
2022-03-01 14:42:51 +0100

    Update BookRepository.java

--- BookRepository.java
+++ BookRepository.java
@@ -2,7 +2,6 @@ package org.sid.Repositories;
 
 import org.sid.Model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Repository;
 
 @Repository
